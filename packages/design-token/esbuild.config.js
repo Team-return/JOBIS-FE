9861730cb9a68fc526e77e533d1e18b133d58605
jikwan <jikwan0327@gmail.com>
2024-01-08 20:06:04 +0900

    fix: esbuild 설정 수정

--- packages/design-token/esbuild.config.js
+++ packages/design-token/esbuild.config.js
@@ -1,13 +1,40 @@
 const esbuild = require('esbuild');
 const { pnpPlugin } = require('@yarnpkg/esbuild-plugin-pnp');
+import pkg from './package.json' assert { type: 'json' };
 
-esbuild
-    .build({
-        entryPoints: ['index.ts'],
-        outdir: 'dist',
+const dev = process.argv.includes('--dev');
+const minify = !dev;
+
+const watch = process.argv.includes('--watch');
+
+const external = Object.keys({
+    ...pkg.dependencies,
+    ...pkg.peerDependencies,
+});
+
+const baseConfig = {
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    minify,
+    sourcemap: true,
+    outdir: 'dist',
+    target: 'es2019',
+    watch,
+    external,
+    plugins: [pnpPlugin()],
+};
+
+Promise.all([
+    esbuild.build({
+        ...baseConfig,
         format: 'cjs',
-        bundle: true,
-        platform: 'node',
-        plugins: [pnpPlugin()],
-    })
-    .catch(() => process.exit(1));
+    }),
+    esbuild.build({
+        ...baseConfig,
+        format: 'esm',
+        outExtension: { '.js': '.cjs' },
+    }),
+]).catch(() => {
+    console.error('Build failed');
+    process.exit(1);
+});
