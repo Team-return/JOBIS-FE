d4de52daa8cf546c362f835df920abaee15f7892
jikwan <jikwan0327@gmail.com>
2024-02-05 17:03:59 +0900

    feat :: text 컴포넌트 추가

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,3 @@
+export * from "./checkbox";
+export * from "./texts";
+export * from "./icon";
---
+++ packages/ui/src/texts/types.ts
@@ -0,0 +1,13 @@
+import { themes } from "@jobis/design-token";
+import { CSSProperties } from "@vanilla-extract/css";
+import { AsElementProps, ColorType } from "../core/types";
+
+export type TextProps = AsElementProps & {
+  color?: string;
+  background?: string;
+  fontSize: keyof typeof themes.font;
+  fontWeight?: keyof typeof themes.fontWeight;
+  align?: CSSProperties["textAlign"];
+  casing?: CSSProperties["textTransform"];
+  decoration?: CSSProperties["textDecoration"];
+};
