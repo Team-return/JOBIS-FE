8a430769c197e8f6bd99900b3fc7a4c64e572dc6
JJIIIINN <kgb21537@gmail.com>
2024-02-06 16:35:57 +0900

    fix :: prettier

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,13 +1,13 @@
 module.exports = {
-    printWidth: 80, // 한 줄 최대 글자 수
-    tabWidth: 2, // 탭 하나당 띄어쓰기 개수
-    useTabs: false, // 탭 사용 X
-    semi: true, // 세미콜론 사용
-    singleQuote: false, // ' ' 사용
-    quoteProps: 'consistent',
-    jsxBracketSameLine: true, // JSX 태그를 사용하는경우에 > 기호를 한줄 내려서 사용
-    bracketSpacing: true, // 객체의 양 끝 간격 사용
-    arrowParens: 'avoid', // 매개변수가 1개 이더라도 소괄호 필수
-    trailingComma: 'es5', // 배열, 객체등의 마지막 요소에 콤마
-    quoteProps: 'as-needed', // 객체의 속성 표현 시, 필요시에만 따옴표 적용
+  printWidth: 80, // 한 줄 최대 글자 수
+  tabWidth: 2, // 탭 하나당 띄어쓰기 개수
+  useTabs: false, // 탭 사용 X
+  semi: true, // 세미콜론 사용
+  singleQuote: false, // ' ' 사용
+  quoteProps: "consistent",
+  jsxBracketSameLine: true, // JSX 태그를 사용하는경우에 > 기호를 한줄 내려서 사용
+  bracketSpacing: true, // 객체의 양 끝 간격 사용
+  arrowParens: "avoid", // 매개변수가 1개 이더라도 소괄호 필수
+  trailingComma: "es5", // 배열, 객체등의 마지막 요소에 콤마
+  quoteProps: "as-needed", // 객체의 속성 표현 시, 필요시에만 따옴표 적용
 };
