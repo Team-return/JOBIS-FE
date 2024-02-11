19388c4c61e8e155313cb617db094c024e1056f9
KANGYONGSU23 <edc20203@gmail.com>
2024-02-11 22:25:28 +0900

    chore :: 타입 옵셔널 처리

--- packages/ui/src/radio/types.ts
+++ packages/ui/src/radio/types.ts
@@ -2,17 +2,16 @@ import React from "react";
 import { Radio } from './index'
 
 export interface RadioType {
-  checked: boolean;
+  checked?: boolean;
   children?: React.ReactNode;
-  onClick: () => void;
+  onClick?: () => void;
   value: string;
 }
 
 export interface RadioGroupType {
   children: React.ReactElement<typeof Radio> | React.ReactElement<typeof Radio>[];
   direction: "column" | "row";
-  className: string;
-  defalutValue: number;
+  className?: string;
   value: string;
   setValue: React.Dispatch<React.SetStateAction<string>>
 }
