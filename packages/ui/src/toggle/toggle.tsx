cc249e1a73b86aef76ab935e7a72e732308a3d67
KANGYONGSU23 <edc20203@gmail.com>
2024-03-06 23:12:53 +0900

    chore :: forwardRef 감싸기

--- packages/ui/src/toggle/toggle.tsx
+++ packages/ui/src/toggle/toggle.tsx
@@ -3,14 +3,17 @@ import { clsx } from "clsx";
 import { toggleBackgroundStyle, circleStyle } from "./style.css";
 import { ToggleProps } from "./types";
 
-export const Toggle = (props: ToggleProps) => {
-  const { checked, ...rest } = props;
-  return (
-    <div
-      className={clsx([toggleBackgroundStyle({ checked: checked })])}
-      {...rest}
-    >
-      <div className={clsx([circleStyle])} />
-    </div>
-  );
-};
+export const Toggle = React.forwardRef(
+  (props: ToggleProps, ref: React.Ref<HTMLDivElement>) => {
+    const { checked, ...rest } = props;
+    return (
+      <div
+        className={clsx([toggleBackgroundStyle({ checked: checked })])}
+        ref={ref}
+        {...rest}
+      >
+        <div className={clsx([circleStyle])} />
+      </div>
+    );
+  }
+);
