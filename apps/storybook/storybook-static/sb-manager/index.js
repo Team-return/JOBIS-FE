5db0b26d9a1d6127879016e51f953869c6a49615
KANGYONGSU23 <edc20203@gmail.com>
2024-03-07 17:06:48 +0900

    fix :: storybook-static 삭제

--- apps/storybook/storybook-static/sb-manager/index.js
+++
@@ -1,6 +0,0 @@
-import "./chunk-2IXBUOFS.js";
-import "./chunk-NGTUFCUO.js";
-import "./chunk-INSKDKQB.js";
-import "./chunk-ZEU7PDD3.js";
-
-export { Provider, renderStorybookUI, Root } from "./chunk-NMB3SATH.js";
