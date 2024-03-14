ecc7591ccc80d01a6d640fae3b2d825e6680c32b
jikwan <jikwan0327@gmail.com>
2024-03-14 16:55:25 +0900

    feat :: 모집의뢰서 수정 페이지 추가

--- apps/company/src/apis/recruitments/index.ts
+++ apps/company/src/apis/recruitments/index.ts
@@ -2,42 +2,61 @@ import { instance } from "../axios";
 import {
   IArea,
   IEditRecruitmentRequest,
+  IMyRecruitmentListResponse,
   IMyRecruitmentResponse,
   IRecruitment,
 } from "./types";
 
 const router = "/recruitments";
 
-export const createRecruitmentsRequest = async (
-  body: IRecruitment,
-  companyId: string
-) => {
-  const { data } = await instance.post<IRecruitment>(
-    `${router}/${companyId}`,
-    body
-  );
+export const createRecruitmentsRequest = async (body: IRecruitment) => {
+  const { data } = await instance.post<IRecruitment>(`${router}`, body);
   return data;
 };
 
 export const myRecruitment = async () => {
-  const { data } = await instance.get<IMyRecruitmentResponse>(`${router}/my`);
+  const { data } = await instance.get<IMyRecruitmentResponse>(
+    `${router}/my/recent`
+  );
+  return data;
+};
+
+export const myRecruitmentList = async () => {
+  const { data } = await instance.get<IMyRecruitmentListResponse>(
+    `${router}/my`
+  );
   return data;
 };
 
 export const updateRecruitment = async (
   body: IEditRecruitmentRequest,
-  id: number
+  recruitmentId: string
 ) => {
-  const { data } = await instance.patch<IRecruitment>(`${router}/${id}`, body);
-  return data;
+  const response = await instance.patch<IRecruitment>(
+    `${router}/${recruitmentId}`,
+    body
+  );
+  return response;
 };
 
-export const addRecruitArea = async (body: IArea, recruitmentId: number) => {
-  const { data } = await instance.post(`${router}/${recruitmentId}/area`, body);
-  return data;
+export const addRecruitArea = async (body: IArea, recruitmentId: string) => {
+  const response = await instance.post(`${router}/${recruitmentId}/area`, body);
+  return response;
 };
 
 export const updateRecruitArea = async (body: IArea, id: number) => {
-  const { data } = await instance.patch(`${router}/area/${id}`, body);
+  const response = await instance.patch(`${router}/area/${id}`, body);
+  return response;
+};
+
+export const deleteRecruitArea = async (recruitAreaId: number) => {
+  const response = await instance.delete(`${router}/area/${recruitAreaId}`);
+  return response;
+};
+
+export const recruitmentDetail = async (recruitmentId: string) => {
+  const { data } = await instance.get<IMyRecruitmentResponse>(
+    `${router}/${recruitmentId}`
+  );
   return data;
 };
