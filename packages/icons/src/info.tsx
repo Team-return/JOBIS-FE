c4b28c0e369181be94b48d3a6dcfc0bb4b716021
jikwan <jikwan0327@gmail.com>
2024-03-18 09:33:33 +0900

    fix :: svg props 카멜 케이스로 수정

--- packages/icons/src/info.tsx
+++ packages/icons/src/info.tsx
@@ -9,7 +9,7 @@ export const Info = () => {
         r="9"
         stroke="currentColor"
         fill="transparent"
-        stroke-width="2"
+        strokeWidth="2"
       />
       <circle cx="12" cy="8" r="1" fill="currentColor" />
       <rect x="11" y="11" width="2" height="6" rx="1" fill="currentColor" />
