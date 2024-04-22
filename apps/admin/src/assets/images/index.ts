36af4b73b06a6ef2648403b342dc855db8da17ce
JJIIIINN <kgb21537@gmail.com>
2024-04-22 15:46:52 +0900

    feat :: 이미지 추가

--- apps/admin/src/assets/images/index.ts
+++ apps/admin/src/assets/images/index.ts
@@ -1,1 +1,7 @@
 export { default as LoadIcon } from "./loadIcon.svg";
+export * from "./chevron";
+export { default as SearchIcon } from "./search.svg";
+export { default as ArrowIcon } from "./arrow.svg";
+export { default as RollbackIcon } from "./rollback.svg";
+
+export { default as Logo } from "./logo.png";
