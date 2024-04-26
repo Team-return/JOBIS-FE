17f4a239139df6696af0145a549e8f535a593305
JJIIIINN <kgb21537@gmail.com>
2024-04-26 10:27:02 +0900

    feat :: 이미지 파일 생성 및 수정

--- apps/admin/src/assets/images/index.ts
+++ apps/admin/src/assets/images/index.ts
@@ -1,7 +1,11 @@
-export { default as LoadIcon } from "./loadIcon.svg";
 export * from "./chevron";
+export * from "./rollback";
+
+export { default as LoadIcon } from "./loadIcon.svg";
 export { default as SearchIcon } from "./search.svg";
 export { default as ArrowIcon } from "./arrow.svg";
-export { default as RollbackIcon } from "./rollback.svg";
+export { default as Arrow2Icon } from "./arrow2.svg";
+export { default as DoubleArrowIcon } from "./doubleArrow.svg";
+export { default as WaveIcon } from "./wave.svg";
 
 export { default as Logo } from "./logo.png";
