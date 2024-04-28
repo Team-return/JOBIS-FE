2abc9f67cebd7d7672c5e5b6c37b07a294166a14
JJIIIINN <kgb21537@gmail.com>
2024-04-28 21:29:18 +0900

    fix :: env 설정

--- apps/admin/src/@types/types/env.d.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-interface ImportMeta {
-  env: {
-    VITE_BASE_URL: string;
-    VITE_FILE_URL: string;
-    VITE_REQUIRED_LICENSES_ENCODING_KEY: string;
-  };
-}
