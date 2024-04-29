9503eb52112812d98194f7ee16a8b72aeaa80e30
JJIIIINN <kgb21537@gmail.com>
2024-04-29 12:33:50 +0900

    feat :: 이미지 추가

--- apps/admin/src/assets/images/index.ts
+++ apps/admin/src/assets/images/index.ts
@@ -7,5 +7,7 @@ export { default as ArrowIcon } from "./arrow.svg";
 export { default as Arrow2Icon } from "./arrow2.svg";
 export { default as DoubleArrowIcon } from "./doubleArrow.svg";
 export { default as WaveIcon } from "./wave.svg";
+export { default as DownArrowIcon } from "./downArrow.svg";
+export { default as CompanyIcon } from "./company.svg";
 
 export { default as Logo } from "./logo.png";
