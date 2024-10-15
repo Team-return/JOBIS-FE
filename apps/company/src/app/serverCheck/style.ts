4741b4f6387e653cb0f14acb12332a76cbbfa6dd
hyuna <hyuna_0525@dsm.hs.kr>
2024-10-15 10:16:04 +0900

    feat :: 서버 점검 페이지 퍼블리싱

---
+++ apps/company/src/app/serverCheck/style.ts
@@ -0,0 +1,16 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100dvh;
+  gap: 12px;
+`;
+
+export const TitleWrap = styled.div`
+  display: flex;
+  gap: 4px;
+`;
