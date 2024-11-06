7493cb89cd5e1bc1c792584d64d2c25dd9fc9935
이의진 <aghpt04@gmail.com>
2024-11-06 22:50:37 +0900

    feat :: 로그아웃 기능 추가

--- apps/company/src/components/modal/logout/logoutModal.tsx
+++ apps/company/src/components/modal/logout/logoutModal.tsx
@@ -4,11 +4,21 @@ import React from "react";
 import * as S from "./style";
 import { Text, Button } from "@jobis/ui";
 import { themes } from "@jobis/design-token";
-import Link from "next/link";
 import { useModal } from "@/hooks/useModal";
+import { Cookies } from "react-cookie";
 
 const LogoutModal = () => {
   const { closeModal } = useModal();
+  const cookies = new Cookies();
+
+  const handleLogout = () => {
+    cookies.remove("access_token");
+    cookies.remove("refresh_token");
+
+    window.location.href = "/";
+
+    closeModal();
+  };
   return (
     <S.Container>
       <S.TextWrapper>
@@ -31,7 +41,12 @@ const LogoutModal = () => {
         >
           취소
         </Button>
-        <Button style={{ width: "112px", height: "48px" }}>확인</Button>
+        <Button
+          onClick={handleLogout}
+          style={{ width: "112px", height: "48px" }}
+        >
+          확인
+        </Button>
       </S.ButtonWrapper>
     </S.Container>
   );
